d8528d52f29af51195c97545c4b3c9494c1601ca
Jacek Trochowski <j.trochowski@gromar.eu>
2023-11-19 22:25:01 +0100

    setup for installed symfony

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-COMPOSER_MEMORY_LIMIT=-1 composer create-project symfony/skeleton symfony
-mv  -fv ./symfony/* ./
-rm -fR ./symfony
-mkdir ./data
-chmod 777 -R ./data
+COMPOSER_MEMORY_LIMIT=-1 composer update --no-interaction
